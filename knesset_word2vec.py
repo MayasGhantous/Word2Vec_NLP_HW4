c80fa25600aa227e43c83c20b8ac36a837fe4904
MayasGhantous <102159871+MayasGhantous@users.noreply.github.com>
2024-03-06 00:39:49 +0200

    push

--- knesset_word2vec.py
+++ knesset_word2vec.py
@@ -1,9 +1,10 @@
 import pandas as pd
 import numpy as np
-import os
-
-
+from sklearn.metrics.pairwise import cosine_similarity
 from gensim.models import Word2Vec
+
+import os
+import heapq
 
 def make_list_of_sentence(sentnce):
     tokens = sentnce.strip().split(' ')
@@ -47,15 +48,19 @@ def Section2_part1(dir,model):
 
 
 def embeddings_of_sentences(sententces,model):
-    sentence_dir = {}
+    sentence_dir = []
+    above_4_and_under_10 =[]
     for i,sentence in enumerate(sententces):
         tokens = make_list_of_sentence(sentence)
+        if len(tokens) >=4 and len(tokens) <=10:
+            above_4_and_under_10.append(i)
         avarage = np.zeros(100)
         for token in tokens:
             avarage += model.wv[token]
         avarage = avarage / len(token)
-        sentence_dir[i] = avarage
-    return sentence_dir
+        sentence_dir.append(avarage)
+    sentence_dir = np.array(sentence_dir)
+    return sentence_dir,above_4_and_under_10
             
 
 
@@ -64,6 +69,7 @@ def embeddings_of_sentences(sententces,model):
 
 if __name__ == '__main__':
 
+    dir = ''
     #section 1 part 1
     data = pd.read_csv('knesset_corpus.csv')
     tokenized_sentences  = make_list(data['sentence_text'])
@@ -76,5 +82,21 @@ if __name__ == '__main__':
 
     #section 2
     #section 2, part 1
-    Section2_part1('',model)
+    Section2_part1(dir,model)
+
     #section 2, part 2
+    sentence_embeddings, allowed_sentences = embeddings_of_sentences(data['sentence_text'],model)
+    #sentences_index = allowed_sentences[:10]
+    #sentences_index = [330,166,369,465,527,553,680,1168,1263,1331]
+    sentences_index = [329,165,368,464,526,552,679,1167,1262,1330]
+    text = ""
+    our_index_embeddings = sentence_embeddings[sentences_index]
+    matrix = cosine_similarity(our_index_embeddings,sentence_embeddings)
+    for i,index in enumerate(sentences_index):
+        text+=data.iloc[index]['sentence_text']+': most similar sentence: '
+        max_index = matrix[i].argsort()[-2]
+        text+=data.iloc[max_index]['sentence_text']+'\n'
+    with open(os.path.join(dir,'knesset_similar_sentences.txt'),'w',encoding='utf-8') as file:
+        file.write(text)
+    
+
