016beacd2230bda734983385cf26b2c2805cc0d2
MayasGhantous <102159871+MayasGhantous@users.noreply.github.com>
2024-03-06 22:14:39 +0200

    push

--- knesset_dictabert.py
+++ knesset_dictabert.py
@@ -1,17 +1,38 @@
 from transformers import AutoModelForMaskedLM, AutoTokenizer
 
-tokenizer = AutoTokenizer.from_pretrained('dicta-il/dictabert')
-model = AutoModelForMaskedLM.from_pretrained('dicta-il/dictabert')
-file_path = 'masked_sentences.txt'
-model.eval()
 
-with open(file_path,'r',encoding='utf-8') as file:
-    text = file.read()
-    new_text = ''
-    sentences = text.split('\n')
-    for sentence in sentences:
-        new_text +="Original sentence: "+sentence+'\n'
-        sentence_Mask = sentence.replace('[*]','[MASK]')
-        output = model(tokenizer.encode(sentence_Mask, return_tensors='pt'))
-        print(output)
-        
+if __name__ == '__main__':
+    tokenizer = AutoTokenizer.from_pretrained('dicta-il/dictabert')
+    model = AutoModelForMaskedLM.from_pretrained('dicta-il/dictabert')
+    file_path = 'masked_sentences.txt'
+    output_file_path = 'dictabert_results.txt'
+    model.eval()
+
+    with open(file_path,'r',encoding='utf-8') as file:
+        text = file.read()
+        #text = 'בשנת 1948 השלים אפרים קישון את [MASK] בפיסול מתכת ובתולדות האמנות והחל לפרסם מאמרים הומוריסטיים'
+        new_text = ''
+        sentences = text.split('\n')[:-1]
+        for sentence in sentences:
+            token_list = []
+
+            current_sentence = sentence
+            new_text +="Original sentence: "+sentence+'\n'
+            sentence_Mask =''
+            count = sentence.count('[*]')
+            for _ in range(count):
+                current_sentence = current_sentence.replace('[*]','[MASK]',1)
+                MASK_index = current_sentence.split(' ').index('[MASK]')
+                tokenizes = tokenizer.encode(current_sentence, return_tensors='pt')
+                output = model(tokenizes)
+
+
+
+                index = output.logits[0, MASK_index+1, :].argmax()
+                predicted_token = tokenizer.convert_ids_to_tokens([index])[0]
+                current_sentence = current_sentence.replace('[MASK]',predicted_token)
+                token_list.append(predicted_token)
+            new_text += 'DictaBERT sentence: '+current_sentence+'\n'
+            new_text +='DictaBERT tokens: ' +', '.join(token_list) +'\n'
+        with open(output_file_path,'w',encoding='utf-8') as output_file:
+            output_file.write(new_text)
